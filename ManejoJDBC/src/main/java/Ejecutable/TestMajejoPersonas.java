9e5253c71068d02552653cb0ec3faed0cb9611c3
f-testingAcount <fdchb2@gmail.com>
2022-08-14 22:14:30 -0300

    Metodo SQL_UPDATE completo y funcionando

--- ManejoJDBC/src/main/java/Ejecutable/TestMajejoPersonas.java
+++ ManejoJDBC/src/main/java/Ejecutable/TestMajejoPersonas.java
@@ -10,8 +10,13 @@ public class TestMajejoPersonas {
         
         
         //Insertamos un nuevo objeto de tipo Persona
-        Persona otraPersona = new Persona("Philip", "Gibbs", "phg@mail.com", "21453698");
-        personaDao.insertar(otraPersona);
+//        Persona otraPersona = new Persona("Philip", "Gibbs", "phg@mail.com", "21453698");
+//        personaDao.insertar(otraPersona);
+        
+        //Modificar un objeto de persona existente
+        Persona personaModificada = new Persona(4, "Philip Antoni", "Gibbs", "pag@mail.com", "47647364");
+        personaDao.actualizar(personaModificada);
+        
         
         List<Persona> personas = personaDao.seleccionar();
         personas.forEach(persona -> {
--- ManejoJDBC/src/main/java/datos/PersonaDAO.java
+++ ManejoJDBC/src/main/java/datos/PersonaDAO.java
@@ -12,6 +12,8 @@ public class PersonaDAO { //DAO (data access object)
 
     private static final String SQL_SELECT = "SELECT idpersona, nombre, apellido, email, telefono FROM persona"; // Para simplificar esta sentencia se puede colocar SELECT * FROM persona
     private static final String SQL_INSERT = "INSERT INTO persona(nombre, apellido, email, telefono) VALUES(?, ?, ?, ?)";
+    private static final String SQL_UPDATE = "UPDATE persona SET nombre = ?, apellido = ?, email = ?, telefono = ? WHERE idpersona = ?";
+    
     public List<Persona> seleccionar() {
         Connection conn = null;
         PreparedStatement stmt = null; //PreparedStatement es una interface
@@ -59,6 +61,33 @@ public class PersonaDAO { //DAO (data access object)
             stmt.setString(2, persona.getApellido());
             stmt.setString(3, persona.getEmail());
             stmt.setString(4, persona.getTelefono());
+            registros = stmt.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+        finally {
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        return registros;
+    }
+    
+    public int actualizar(Persona persona) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, persona.getNombre());
+            stmt.setString(2, persona.getApellido());
+            stmt.setString(3, persona.getEmail());
+            stmt.setString(4, persona.getTelefono());
+            stmt.setInt(5, persona.getIdPersona());
             registros = stmt.executeUpdate();
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
